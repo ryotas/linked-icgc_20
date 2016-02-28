034b77b184847f0dd7b7e2aaec6940da92f4b876
EC2 Default User <ec2-user@ip-172-31-29-82.ap-northeast-1.compute.internal>
2016-02-28 05:55:08 +0000

    commit

--- 60_load_all.sh
+++ 60_load_all.sh
@@ -2,11 +2,13 @@
 
 date
 
+echo -e "\n\nLoading icgc..."
+time ~/virtuoso.sh loaddir ~/data all_projects.ttl http://hoge/
 echo -e "\n\nLoading hgnc..."
-time ~/virtuoso.sh loaddir ~/linked-icgc/output hgnc_complete_set.nq http://hoge/
+time ~/virtuoso.sh loaddir ~/data hgnc_complete_set.nq http://hoge/
 echo -e "\n\nLoading uniprot..."
-time ~/virtuoso.sh loaddir ~/linked-icgc/output 9606.ttl http://purl.uniprot.org/ 
+time ~/virtuoso.sh loaddir ~/data 9606.ttl http://purl.uniprot.org/ 
 echo -e "\n\nLoading reactome..."
-time ~/virtuoso.sh loaddir ~/linked-icgc/output Homo_sapiens.owl http://purl.uniprot.org/ 
+time ~/virtuoso.sh loaddir ~/data Homo_sapiens.owl http://purl.uniprot.org/ 
 
 date
